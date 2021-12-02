619419333f3cfa9c3092351d75139181267a1483
Nikolay Abadeev <voood234@gmail.com>
2021-12-02 18:38:37 +0300

    Add

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int prise) {
+        int price = 24_500;
+        int amount = 20;
+        int bonus = price/amount;
+        return bonus;
+    }
+}
